07f9e81cf0a8727cc3984c47976fb8ec43d5194a
Kay Holten <holtenkay@gmail.com>
2021-01-23 13:38:16 -0800

    Creating Pairs solved

--- creatingPairs.js
+++ creatingPairs.js
@@ -12,3 +12,9 @@ function createPairs(arr) {
 
 console.log(createPairs([1, 2, 3, 4, 4]))
 console.log(createPairs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]))
+
+/*
+
+The Big O notation is O(n^2) because there's a for loop nested inside another for loop.
+
+*/
