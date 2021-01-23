11e93f096c76a7a90992507088ffd207fd0f3c0c
Kay Holten <holtenkay@gmail.com>
2021-01-23 13:54:23 -0800

    Efficient Search solved

---
+++ efficientSearch.js
@@ -0,0 +1,32 @@
+function efficientSearch(array, item) {
+    let minIndex = 0;
+    let maxIndex = array.length - 1;
+    let currentIndex;
+    let currentElement;
+
+    while (minIndex <= maxIndex) {
+        currentIndex = Math.floor((minIndex + maxIndex) / 2);
+        currentElement = array[currentIndex];
+
+        if (currentElement < item) {
+            minIndex = currentIndex + 1;
+        }
+        else if (currentElement > item) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+    return -1;
+}
+
+console.log(efficientSearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], 17))
+console.log(efficientSearch([1, 2, 3, 4], 6))
+
+/*
+
+The Big O is O(log n) becaues line 8 halves the array you're working with each time by finding the midpoint
+and going from there.
+
+*/
