7ef052f4533d524a2176c2349e93257f1b967f62
Kay Holten <holtenkay@gmail.com>
2021-01-23 16:31:55 -0800

    Reverse String solved

---
+++ nowIterate/reverseString.js
@@ -0,0 +1,13 @@
+function reverseString(string) {
+    let newString = "";
+
+    for (let i = 0; i < string.length; i++) {
+        newString = string[i] + newString
+    }
+    return newString
+}
+
+console.log(reverseString("p"))
+console.log(reverseString("pneumonia"))
+console.log(reverseString("pneumonultramicroscopicsilicavolicanosis"))
+console.log(reverseString("apple"))
